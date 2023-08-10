4a4c3fda08aecd456d4f3aa8837c1a97b76d4cb2
Dhammdip Modak <124656648+dhammu10@users.noreply.github.com>
2023-08-10 22:49:46 +0530

    Create Life_in_weeks.py

---
+++ Life_in_weeks.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+year = 90 - int(age)
+Days = round(year*365)
+Weeks = round(year*52)
+months = round(year*12)
+print (f"You have {Days} days, {Weeks} weeks, and {months} months left.")
